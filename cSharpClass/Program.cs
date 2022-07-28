edbed69c15a32ed50c9b5acb64a1ccc6daf61da2
Jeni Maharjan <jenny_mahar@hotmail.com>
2022-07-28 09:55:41 +0545

    first assignment

---
+++ cSharpClass/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.Write("Write any text to change in upper:");
+string Name = Console.ReadLine();
+string outCapName =Name.ToUpper();
+Console.WriteLine($"Converted output is: , {outCapName}");
+
+Console.Write("Enter data in Meter: ");
+string valMeter = Console.ReadLine();
+double valFeet = Int32.Parse(valMeter) * 3.2808399;
+Console.WriteLine($"Converted output is:  {valFeet}");
+
+
+
