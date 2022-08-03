fdcf8767d76ded9c2b6017bdfb1520ab82804d9c
Jeni Maharjan <jenny_mahar@hotmail.com>
2022-08-03 10:21:47 +0545

    min,max and avg value

--- cSharpClass/EntryPoint.cs
+++ cSharpClass/EntryPoint.cs
@@ -1,16 +1,36 @@
+using System;
 class TestClasses
 {
     public static void Main()
     {
-        Animal cow = new Animal();// old version
-        Animal goat = new ();  //new version
-        var buffalo = new Animal(); // if declared as var
-        Animal deer = new Animal();
-        Animal fish = new Animal();
-        Animal bear = new Animal();
+        // Animal cow = new Animal();// old version
+        // Animal goat = new ();  //new versionls
+        // var buffalo = new Animal(); // if declared as var
+        // Animal deer = new Animal();
+        // Animal fish = new Animal();
+        // Animal bear = new Animal();
 
-        cow.weight =325.4f;
-        cow.type = "Herbivorous animal cow";
-        cow.PrintDetails();
+        // cow.weight =325.4f;
+        // cow.type = "Herbivorous animal cow";
+        // cow.PrintDetails();
+
+        // BranchesAndLoops br = new();
+        // br.learnBranching();
+        // br.LearnLooping();
+
+        // Assignment2 assignment2 = new();
+        // assignment2.PrintPattern();
+        //squiggle the term used while code shows error line
+         MethodLearning ml = new();
+        // ml.PrintNepal();
+        // ml.PrintNepalNTimes(10);
+        // var x = ml.GetInitials("Milan sfafsdf Dhakal");
+        // Console.WriteLine(x);
+
+        short[] numbers = {45,25,26,28,65,48,82,-12,-20,-1,0}; //numbers varible name is not same as declared in class methods as it's in another class
+        //var (min,max) = ml.GetMinMax(numbers);
+        var (min,max,avg) = ml.GetMinMaxAvg(numbers);
+        Console.WriteLine($"Minimum: {min}, Maximum :{max} and Average Value : {avg}");
+        
     }
 }
---
+++ cSharpClass/Methods.cs
@@ -0,0 +1,73 @@
+using System;
+
+public class MethodLearning
+{
+    // returns nothing, take no arguments
+    public void PrintNepal()
+    {
+        Console.WriteLine("Nepal");
+    }
+    // returns nothing, take some arguments
+    public void PrintNepalNTimes(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nepal");
+        }
+    }
+
+    // returns some value, take no/some arguments
+    public int Add(int x, int y)
+    {
+        var sum = x + y;
+        return sum;
+    }
+
+    // input- Bishnu Singh sdfasfd Rawal, output - BR
+    public string GetInitials(string fullName)
+    {
+        var nameParts = fullName.Split(" "); //delimeter
+        var len = nameParts.Length;
+        var first = nameParts[0][0]; //as string is also a kind of string array we can use indexing on string values
+        var last = nameParts[len - 1][0];
+
+        var initial = $"{first}{last}";
+        return initial;
+    }
+
+    // returns multiple values, take no/some arguments
+    public (short,short) GetMinMax(short[] numbers)
+    {
+        short min =short.MaxValue, max=short.MinValue;
+        foreach(short num in numbers)
+        {
+            if (num < min)
+                min = num;
+            if(num>max)
+                max=num;
+        }
+        return (min,max);  //tuple of 2 variales we can use any no of variables in tuples of variables
+    }
+
+    
+    // 1. Write a function to return min, max and average of given array of numbers. Hint GetMinMax() 
+    public (short,short,int) GetMinMaxAvg(short[] numbers)
+    {
+        short min =short.MaxValue, max=short.MinValue;
+        int avg =0;
+        int len = numbers.Length;
+        foreach(short num in numbers)
+        {
+            if (num < min)
+                min = num;
+            if(num>max)
+                max=num;
+            avg = avg + num ;
+        }
+        return (min,max, avg/len);  //tuple of 2 variales we can use any no of variables in tuples of variables
+    }
+
+
+    // variable number of arguments, named parameters, optional parameters 
+
+}
