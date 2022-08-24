490200ef15e14547d96dabbf6fcb1ddaf38dee64
Jeni Maharjan <jenny_mahar@hotmail.com>
2022-08-24 11:00:45 +0545

    20780508updates

---
+++ WebAssignment/Models/Employees.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAssignment.Models;
+
+public class Employees
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Designation { get; set; }
+    public byte Level { get; set; }
+    public DateTime JoinDate { get; set; }
+    public string Department { get; set; }
+}
+
+
