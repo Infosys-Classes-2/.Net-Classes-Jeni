df7bc504db6b9bb948eea63f5545575cc1ba76e0
Jeni Maharjan <jenny_mahar@hotmail.com>
2022-09-21 17:14:57 +0545

    new starting from HRM folder

---
+++ HRM/HRM.Web/HRM.Web/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace HRM.Web.Models;
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Designation { get; set; }
+    public byte Level { get; set; }
+    public DateTime JoinDate { get; set; }
+
+    // One employee has only one department, department has multiple employees
+    public int DepartmentId { get; set; }
+    public Department Department { get; set; }
+}
